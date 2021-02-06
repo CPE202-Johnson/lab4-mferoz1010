bf4ad17d27d09e7abfe98da44cea9f8fd6876021
mferoz1010 <77012139+mferoz1010@users.noreply.github.com>
2021-02-05 17:45:46 -0800

    Update ordered_list_tests.py

--- ordered_list_tests.py
+++ ordered_list_tests.py
@@ -27,6 +27,8 @@ class TestLab4(unittest.TestCase):
         t_list.add(7)
         self.assertEqual(t_list.python_list(), [3,5,7,10])
         self.assertEqual(t_list.index(5), 1)
+        self.assertEqual(t_list.index(6), None)
+        self.assertEqual(t_list.python_list_reversed(), [10, 7, 5, 3])
         self.assertTrue(t_list.remove(7))
         self.assertTrue(t_list.remove(3))
 
@@ -36,6 +38,12 @@ class TestLab4(unittest.TestCase):
         self.assertEqual(t_list.index(10), None)
         with self.assertRaises(IndexError):
             t_list.pop(-1)
+        self.assertFalse(t_list.search(10))
+        self.assertEqual(t_list.python_list(), [])
+        self.assertEqual(t_list.python_list_reversed(), [])
+        t_list.add(10)
+        self.assertEqual(t_list.pop(0), 10)
+        self.assertTrue(t_list.search(10))
 
 
 if __name__ == '__main__': 
